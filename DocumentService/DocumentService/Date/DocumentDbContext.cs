af58eb6d5b7ecca11643ef9dcd4a8395e2156d5a
AlekseyGrogSan <alexkernel05@gmail.com>
2026-08-21 12:40:56 +0300

    Add DbContext Settings

--- DocumentService/DocumentService/Date/DocumentDbContext.cs
+++ DocumentService/DocumentService/Date/DocumentDbContext.cs
@@ -1,6 +1,24 @@
-﻿namespace DocumentService.Date
+﻿using Microsoft.EntityFrameworkCore;
+using DocumentService.Core.Entities;
+using System.Reflection;
+
+namespace DocumentService.Date
 {
-    public class DocumentDbContext
+    public class DocumentDbContext : DbContext
     {
+        public DocumentDbContext(DbContextOptions<DocumentDbContext> options) : base(options) { }
+
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<ReportRequest> ReportRequests { get; set; }
+        public DbSet<ReportRequestDocument> ReportRequestDocuments { get; set; }
+        public DbSet<Report> Reports { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
     }
 }
